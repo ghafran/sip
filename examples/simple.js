6915f050942f340cd41363789a4b82daf0194368
Ghafran Abbas <ghafranabbas@gmail.com>
2014-12-27 18:55:21 -0500

    asd

--- examples/simple.js
+++ examples/simple.js
@@ -5,13 +5,28 @@ var db = {};
 
 app.register(function (req, res) {
 
-    db[req.user.address] = {
-        domain: req.register.domain,
-        address: req.user.address,
-        contact: req.user.contact
-    };
-    
-    res.send(200);
+    // get requesting username
+    var username = req.auth.username;
+
+    // get password from database for username
+    var password = 'password';
+
+    // validate password
+    var valid = req.auth.validate({
+        password: 'password'
+    });
+
+    if (valid) {
+        db[req.user.address] = {
+            domain: req.register.domain,
+            address: req.user.address,
+            contact: req.user.contact
+        };
+
+        res.status(200).send();
+    } else {
+        res.status(401).send();
+    }
 });
 
 app.listen();
