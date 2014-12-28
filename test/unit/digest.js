6086f0ac61b0b2094718d3b4623fe116048d9c1a
ghafran <ghafranabbas@gmail.com>
2014-12-28 14:23:11 -0500

    d

--- test/unit/digest.js
+++ test/unit/digest.js
@@ -10,7 +10,10 @@ describe('Digest', function () {
 
         it('validate', function (done) {
 
-            var isValid = Digest.validateAuthorization('Digest realm="host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41", username="test",  uri="/dir/index.html", response="6629fae49393a05397450978507c4ef1", cnonce="0a4f113b", nc=00000001, qop=auth',
+            var isValid = Digest.validateAuthorization('Digest realm="host.com", ' +
+            'nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41", ' +
+            'username="test",  uri="/dir/index.html", response="6629fae49393a05397450978507c4ef1", ' +
+            'cnonce="0a4f113b", nc=00000001, qop=auth',
             'host.com', 'Mufasa', 'Circle Of Life', 'GET', '');
             xpect(isValid).to.equal(true);
             done();
